a183336de852f189e3797957d0d022a9573314d9
unknown <tess@coralboards.org>
2022-01-04 22:30:16 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+gsap.to(".logo",{
+    y:60,
+    skewX:"-25deg",
+    duration:3,
+});
+
+gsap.to(".nav",{
+    x: -120,
+    duration: 2,
+});
+
+gsap
---
+++ js/app.js
@@ -0,0 +1,11 @@
+
+gsap.to(".nav",{
+    x: -120,
+    duration: 2,
+});
+
+gsap.from(".date",{
+    y:120,
+    opacity:0,
+    duration:2,
+})
